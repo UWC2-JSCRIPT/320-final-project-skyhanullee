82ae37a14f50ba9527746ae434c124299f0dae17
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-02-27 05:16:36 -0800

    started working on pokedex logic to show multiple cards of pokemon

--- pokemon-viewer/src/HomePage.jsx
+++ pokemon-viewer/src/HomePage.jsx
@@ -1,30 +1,35 @@
 import PokemonViewer from "./PokemonViewer";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import PokemonCard from "./PokemonCard";
+// import test_data from './data.json';
 
 function HomePage() {
-  const [searchTerms, setSearchTerms] = useState('');
+  const [searchTerms, setSearchTerms] = useState('bulbasaur');
   const [location, setLocation] = useState('');
 
-  const [pokemon, setPokemon] = useState('');
-  const [pokemonId, setPokemonId] = useState(0);
+  // const [pokemon, setPokemon] = useState('');
   const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonDataList, setPokemonDataList] = useState([]);
 
   const [loading, toggleLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
 
+  const [resultsPerPage, setResultsPerPage] = useState();
+
+  console.log(pokemonData);
+
   const onFormSubmit = (event) => {
     event.preventDefault();
     setSearchTerms(event.target[0].value);
-    setLocation(event.target[1].value);
+    // getPokemonData(searchTerms);
+    // event.target[0].value = '';
   }
 
   // useEffect(() => {
-  //   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  //   fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerms}`)
   //   .then(response => response.json())
-  //   .then(
-  //     (data) => {
-  //       // console.log(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${API_ID}&app_key=${API_KEY}&results_per_page=${resultsPerPage}&what=${searchTerms}&where=${location}`);
+  //   .then((data) => {
   //       setPokemonData(data);
   //       toggleLoading(false);
   //     },
@@ -33,15 +38,15 @@ function HomePage() {
   //       setHasError(true);
   //     }
   //   )
-  // }, [pokemon]);
+  // }, [searchTerms]);
 
-  const getPokemonData = (pokemon) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${resultsPerPage}&offset=0`)
     .then(response => response.json())
     .then((data) => {
-        let tempId = (`00${data.id}`).slice(-3);
-        setPokemonId(tempId)
-        setPokemonData(data);
+      // console.log(data);
+      setPokemonDataList(data);
+      // console.log(pokemonDataList);
         toggleLoading(false);
       },
       (error) => {
@@ -49,7 +54,7 @@ function HomePage() {
         setHasError(true);
       }
     )
-  }
+  }, [resultsPerPage]);
 
   if (loading) {
     return <p>loading...</p>
@@ -59,19 +64,43 @@ function HomePage() {
     return <p>Error</p>
   }
 
-  <form onSubmit={onFormSubmit}>
-    <div id='form-search-pokemon'>
-      <label htmlFor='pokemon-search'>Search: </label>
-      <input type='text' id='pokemon-search-input' placeholder='Pokemon' value={searchTerms} />
-    </div>
-  </form>
+  const pokemonArray = pokemonDataList.results.map((pokemon, index) => {
+    // console.log(pokemon.name);
+    return (
+      <li key={index}>
+        {/* <Link to={`/pokemon/${pokemon.id}`} state={{ data: { pokemon } }}> */}
+          <PokemonCard pokemon={pokemon} />
+        {/* </Link> */}
+      </li>
+    )
+  })
+
 
   return (
     <div className="">
       <header className="App-header">
         Pokemon Viewer
-        <PokemonViewer />
+        <form onSubmit={onFormSubmit}>
+          <div id='form-search-pokemon'>
+            <label htmlFor='pokemon-search'>Search: </label>
+            <input type='text' id='pokemon-search-input' placeholder='Pokemon'/>
+          </div>
+          <div id='form-results-per-page'>
+            <label htmlFor='job-search'>Results per page: </label>
+            <input type='number' id='results_per_page-input' min='1' defaultValue='4' value={resultsPerPage} />
+          </div>
+          <button type='submit'>
+          Search
+            {/* <Icon icon='material-symbols:search-rounded' className='search-icon' /> */}
+          </button>
+        </form>
       </header>
+      {/* <PokemonViewer pokemonData={pokemonData} /> */}
+      <section className="pokemon-card-container">
+        <ul>
+          {pokemonArray}
+        </ul>
+      </section>
     </div>
   )
 }
