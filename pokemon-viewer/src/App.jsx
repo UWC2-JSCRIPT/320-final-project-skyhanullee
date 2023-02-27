6fefdd27878dcf61d4b87d3a6ab98ead4af58794
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-02-27 05:16:10 -0800

    added pokedex route

--- pokemon-viewer/src/App.jsx
+++ pokemon-viewer/src/App.jsx
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import PokemonApp from './PokemonApp';
 import HomePage from './HomePage';
 import PokemonPage from './PokemonPage';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+        <Navbar />
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/pokedex' element={<PokemonPage />} />
           <Route path='/pokemon/:pokemonId' element={<PokemonPage />} />
         </Routes>
     </div>
