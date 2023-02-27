df3701f26099c2935cf1a609b9ab652de0bf52b2
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-02-27 05:17:13 -0800

    initial commit

--- pokemon-viewer/src/PokemonPage.jsx
+++ pokemon-viewer/src/PokemonPage.jsx
@@ -1,6 +1,63 @@
+import './PokemonPage.css';
+import { useLocation, useParams } from 'react-router-dom';
+
 function PokemonPage() {
+  // const { pokemonId } = useParams();
+  const dataLocation = useLocation();
+
+  const pokemonObject = dataLocation.state?.data;
+
+  const { name, id, sprites, abilities, types, stats } = pokemonObject.pokemonData;
+
+  const abilityList = abilities.map((ability, index) => {
+    return (
+      <li key={index}>
+        {ability.ability.name}
+      </li>
+    )
+  })
+
+  const typeList = types.map((type, index) => {
+    return (
+      <li key={index} className='pokemon-type'>
+        {type.type.name}
+      </li>
+    )
+  })
+
+  const statsList = stats.map((stat, index) => {
+    return (
+      <li key={index}>
+        {stat.stat.name}: {stat.base_stat}
+      </li>
+    )
+  })
+
+
   return (
-    <div>PokemonPage</div>
+    <section>
+      <div className="pokemon-image-container">
+        <img src={sprites.other['official-artwork']['front_default']} alt={`${name}'s official artwork`} />
+      </div>
+      
+      <div className="pokemon-details">
+        <div className="pokemon-title">
+          <h1>Name: {name}</h1>
+          <h2>ID: {id}</h2>
+        </div>
+        <div className="pokemon-types">
+          {typeList}
+        </div>
+        <div className="pokemon-abilities">
+          <h2>Ability List</h2>
+          {abilityList}
+        </div>
+        <div className="pokemon-stats">
+          <h2>Stats</h2>
+          {statsList}
+        </div>
+      </div>
+    </section>
   )
 }
 export default PokemonPage
--- pokemon-viewer/src/PokemonViewer.jsx
+++ pokemon-viewer/src/PokemonViewer.jsx
@@ -1,8 +1,18 @@
-function PokemonViewer() {
+import { Link } from "react-router-dom";
+
+function PokemonViewer({ pokemonData }) {
+  const { name, id, sprites } = pokemonData;
+  // console.log(sprites.other['official-artwork']);
+
   return (
-    <div>
-      
-    </div>
+    <section className="pokemon-viewer-container">
+      <h1>Name: {name}</h1>
+      <h2>ID: {id}</h2>
+      {/* <img src={sprites.front_default} alt={`${name}'s sprite`} /> */}
+      <Link to={`pokemon/${id}`} state={{ data: { pokemonData } }}>
+        <img src={sprites.other['official-artwork']['front_default']} alt={`${name}'s official artwork`} />
+      </Link>
+    </section>
   )
 }
 export default PokemonViewer
