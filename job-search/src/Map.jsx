fb8adcb924041e7015bdb09680b7f0b0efcfa0c9
skyhanullee <leeskyha@gmail.com>
2023-03-06 16:04:20 -0800

    remvoed alteredListOfJobs and used jobResult directly

--- job-search/src/Map.jsx
+++ job-search/src/Map.jsx
@@ -1,8 +1,8 @@
 import { useRef, useEffect, useState } from "react";
 
-function Map({ center, zoom, markerCoordinateArray, alteredListOfJobs }) {
+function Map({ center, zoom, jobResult }) {
   const ref = useRef();
-  // console.log(alteredListOfJobs);
+  // console.log(jobResult.results);
 
   useEffect(() => {
     let map = new window.google.maps.Map(ref.current, {
@@ -10,23 +10,29 @@ function Map({ center, zoom, markerCoordinateArray, alteredListOfJobs }) {
       zoom,
     });
 
-    Array.from(alteredListOfJobs).forEach((job) => {
-      if(!isNaN(job.location.lat)) {
+    
+
+    jobResult.results.forEach((job) => {
+      // console.log(job);
+      if(job.latitude !== undefined) {
         // setMarkerArray([
         //   ...markerArray,
         //   new window.google.maps.Marker({position: job.location, map: map})
         // ])
-        console.log(job.job.title, job.location);
-        new window.google.maps.Marker({position: job.location, map: map});
+        // console.log(job.title, job.latitude, job.longitude);
+        new window.google.maps.Marker({position: {lat: job.latitude, lng: job.longitude}, map: map});
         // new window.google.maps.Marker({position: { lat: 47.6062, lng: -122.3321 } , map: map});
         // new window.google.maps.Marker({position: { lat: 47.6162, lng: -122.3021 } , map: map});
+        const bounds = new window.google.maps.LatLngBounds();
+        bounds.extend(new window.google.maps.LatLng(job.latitude, job.longitude)
+        );
       }
 
     // console.log('map called');
     });
 
 
-  }, [alteredListOfJobs]);
+  }, [jobResult]);
 
 
 
