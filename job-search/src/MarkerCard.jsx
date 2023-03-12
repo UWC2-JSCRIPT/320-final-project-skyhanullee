e8a82834dccfe8d83d1c036b8a4d3b992cf0282a
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-12 06:07:08 -0700

    changed class names to marker

--- job-search/src/MarkerCard.jsx
+++ job-search/src/MarkerCard.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function MarkerCard({ job }) {
   const { title, location, company, salary_min, created } = job
   const dateCreated = new Date(created).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
   const salary = salary_min.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 });
 
   return (
-    <div className='job-card'>
-      <h2 className='job-title'>{title}</h2>
-      <div className='job-details'>
+    <div className='marker-card'>
+      <h2 className='marker-title'>{title}</h2>
+      <div className='marker-details'>
         <h3>Location:</h3>
         <p>{location.display_name}</p>
         <h3>Company:</h3>
@@ -16,6 +18,9 @@ function MarkerCard({ job }) {
         <h3>Date Created: </h3>
         <p>{dateCreated}</p>
       </div>
+      {/* <Link to={`/job/${job.id}`} state={{ data: { job } }}>
+        <button>More Info</button>
+      </Link> */}
     </div>
   )
 }
