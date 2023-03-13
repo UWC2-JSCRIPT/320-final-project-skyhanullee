cce7d77d05f9a33eaba4bc6b260282b37aedc6e3
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-13 05:54:09 -0700

    changed nav links

--- job-search/src/Navbar.jsx
+++ job-search/src/Navbar.jsx
@@ -6,14 +6,17 @@ function Navbar() {
   return (
     <header>
       <nav className='top-nav'>
+        <div className="nav-title-container">
+          <img src={adzunaIcon} alt='adzuna logo' id='adzuna-icon'/>
+          <h2 id='nav-title'>Job Search via Adzuna</h2>
+        </div>
         <ul className='links-container'>
-          <li id='home-button'>
-            <Link to='/'>
-              <Icon icon='material-symbols:home' id='home-icon' />
-            </Link>
+          <li className="nav-link" id='home-link'>
+            <Link to='/'>Home</Link>
           </li>
+          <li className="nav-link" id="sign-in-link">Sign In</li>
+          <li className="nav-link" id="theme-toggle">Theme</li>
         </ul>
-        <h2 id='nav-title'>Job Search via Adzuna</h2><img src={adzunaIcon} alt='adzuna logo' id='adzuna-icon'/>
       </nav>
     </header>
   )
