605b1749705253207e57e58d6b0d7643c06c7088
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-14 03:05:37 -0700

    added ThemeContext

--- job-search/src/App.jsx
+++ job-search/src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
 import JobPage from './JobPage';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
-import MapPage from './MapPage';
+import ThemeContext from './ThemeContext';
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className='App'>
       <Navbar />
       <Routes>
-        {/* <Route path='/' element={<HomePage />} /> */}
         <Route path='/' element={<HomePage />} />
         <Route path='/job/:jobId' element={<JobPage />} />
       </Routes>
