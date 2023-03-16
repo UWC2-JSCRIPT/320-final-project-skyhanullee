6bbd32e4cd005c3fd4a41ec61b3a9c61b9f0023b
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-15 18:45:47 -0700

    added firebase

--- job-search/src/App.jsx
+++ job-search/src/App.jsx
@@ -5,10 +5,19 @@ import JobPage from './JobPage';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
 import ThemeContext, { ThemeController } from './ThemeContext';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
 
 function App() {
   const { themeName } = useContext(ThemeContext);
   console.log(`the theme from App is ${themeName}`);
+
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+  });
 
   return (
     // <div className='App' data-theme={themeName}>
