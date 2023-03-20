3a644620ebde42faf0827045ab7c4ff38b17f402
skyhanullee <leeskyha@gmail.com>
2023-03-20 16:08:08 -0700

    moved Marker functionality into helper file

--- job-search/src/components/Marker.js
+++ job-search/src/components/Marker.js
@@ -1,16 +1,19 @@
+import { useContext } from "react";
 import { renderToString } from "react-dom/server";
+import MapContext from "../context/MapContext";
 import MarkerCard from "./MarkerCard";
 
-function Marker({ job, map }) {
-  console.log(`----------marker called----------`);
-  const { latitude, longitude, title, id } = job;
-  const markerPosition = new window.google.maps.LatLng(latitude, longitude);
+function Marker( job, map, bounds, setMarkerInfowindow ) {
+  // console.log('--------------------Marker is called-------------------------');
+  const { latitude, longitude, id } = job;
+  // const markerPosition = new window.google.maps.LatLng(latitude, longitude);
+  const markerPosition = { lat: latitude, lng: longitude }
   const marker = new window.google.maps.Marker({
     id: id,
     position: markerPosition,
     map: map,
   });
-  console.log(marker);
+  // console.log(marker);
   // new window.google.maps.Marker({position: { lat: 47.6062, lng: -122.3321 } , map: map});
   // new window.google.maps.Marker({position: { lat: 47.6162, lng: -122.3021 } , map: map});
   const infowindow = new window.google.maps.InfoWindow({
@@ -18,6 +21,9 @@ function Marker({ job, map }) {
     content: renderToString(<MarkerCard job={job} />),
     position: markerPosition,
   });
+
+  setMarkerInfowindow(infowindow);
+
   marker.addListener("click", () => {
     // if (infowindow.open) {
     infowindow.open({
@@ -29,10 +35,13 @@ function Marker({ job, map }) {
     //   infowindow.close();
     // }
   });
+  const jobLatLng = new window.google.maps.LatLng(job.latitude, job.longitude);
+  bounds.extend(jobLatLng);
+  map.fitBounds(bounds);
 
 
   return (
-      <>{marker}</>
+    marker
   )
 }
 export default Marker
