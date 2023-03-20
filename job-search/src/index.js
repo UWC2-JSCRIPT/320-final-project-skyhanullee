cf75c9076bfa2306497291e630592c4adfe70a20
skyhanullee <leeskyha@gmail.com>
2023-03-20 16:09:23 -0700

    added Map and Wrapper

--- job-search/src/index.js
+++ job-search/src/index.js
@@ -6,17 +6,45 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeController } from './context/ThemeContext';
 import { JobResultController } from './context/JobResultContext';
+import { MapController } from './context/MapContext';
+import { Wrapper } from "@googlemaps/react-wrapper";
+import Map from './components/Map';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+const Status = {
+  LOADING: 'LOADING',
+  FAILURE: 'FAILURE',
+  SUCCESS: 'SUCCESS',
+}
+
+const render = (status) => {
+  // eslint-disable-next-line default-case
+  switch (status) {
+    case Status.LOADING:
+      // return <Spinner />;
+      return <h1>Loading</h1>
+    case Status.FAILURE:
+      // return <ErrorComponent />;
+      return <h1>Error</h1>
+    case Status.SUCCESS:
+      return <Map />;
+  }
+};
 root.render(
   <React.StrictMode>
     <ThemeController>
-    <JobResultController>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      </JobResultController>
+      <Wrapper apiKey={GOOGLE_MAPS_API_KEY} render={render}>
+        <MapController>
+          <JobResultController>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </JobResultController>
+        </MapController>
+      </Wrapper>
     </ThemeController>
   </React.StrictMode>
 );
