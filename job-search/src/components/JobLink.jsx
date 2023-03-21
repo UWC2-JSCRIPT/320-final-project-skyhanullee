fcf7b2b54f6009410395fbf2680918d84fdfd024
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-21 03:56:55 -0700

    changed to not say job id

--- job-search/src/components/JobLink.jsx
+++ job-search/src/components/JobLink.jsx
@@ -1,7 +1,8 @@
 function JobLink({ jobId }) {
   return (
-    <a href={`https://adzuna.com/details/${jobId}`} className='job-link'>
-      <button>Adzuna Ad Link: {jobId}</button>
+    <a href={`https://adzuna.com/details/${jobId}`} id='job-link'>
+      {/* <button>Adzuna Ad Link: {jobId}</button> */}
+      <button>Link to Adzuna Page</button>
     </a>
   )
 }
