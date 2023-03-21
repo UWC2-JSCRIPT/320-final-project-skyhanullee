8c1d0c9b8004f0ee01c871bded7609fc11d2b14a
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-21 03:57:16 -0700

    removed addMarker and cleaned up unused code

--- job-search/src/components/JobResultList.jsx
+++ job-search/src/components/JobResultList.jsx
@@ -7,44 +7,6 @@ import JobCard from './JobCard';
 import Marker from './Marker';
 import MarkerCard from './MarkerCard';
 
-function addMarker(job, map, setMap, bounds, setBounds) {
-  console.log('addMarker is called');
-  const { latitude, longitude, id } = job;
-  // const markerPosition = new window.google.maps.LatLng(latitude, longitude);
-  const markerPosition = { lat: latitude, lng: longitude }
-  const marker = new window.google.maps.Marker({
-    id: id,
-    position: markerPosition,
-    map: map,
-  });
-  console.log(typeof (marker));
-  // console.log(marker);
-  // new window.google.maps.Marker({position: { lat: 47.6062, lng: -122.3321 } , map: map});
-  // new window.google.maps.Marker({position: { lat: 47.6162, lng: -122.3021 } , map: map});
-  const infowindow = new window.google.maps.InfoWindow({
-    // content: title,
-    content: renderToString(<MarkerCard job={job} />),
-    position: markerPosition,
-  });
-
-  marker.addListener("click", () => {
-    // if (infowindow.open) {
-    infowindow.open({
-      anchor: marker,
-      map,
-    });
-    // }
-    // else {
-    //   infowindow.close();
-    // }
-  });
-
-
-  const jobLatLng = new window.google.maps.LatLng(job.latitude, job.longitude);
-  bounds.extend(jobLatLng);
-  map.fitBounds(bounds);
-}
-
 function JobResultList() {
   const { jobResult } = useContext(JobResultContext);
   const { map, setMap, bounds, setBounds } = useContext(MapContext);
@@ -90,39 +52,31 @@ function JobResultList() {
     // }
     if (job.latitude === undefined) {
       console.log('job.latitude is undefined');
-      // return
     }
     if (map === undefined) {
       console.log('map is undefined');
-      // return
     }
     if (bounds === undefined) {
       console.log('bounds is undefined');
-      // return
     }
-    // if (job.latitude !== undefined && map !== undefined && bounds !== undefined) {
-    //   // addMarker(job, map, setMap, bounds, setBounds);
-    //   let tempMarker = Marker(job, map, setMap, bounds, setBounds);
-    //   // console.log(typeof(tempMarker));
-    //   setMarkerList([...markerList, tempMarker]);
-    // }
+
+    const findJobId = jobMarkerList.find(j => j.id === job.id);
+
 
     return (
       <li key={job.id} onClick={() => {
         console.log('job result list onclick');
-        const checkJobId = jobMarkerList.find(j => j.id === job.id);
-          console.log(checkJobId);
-        if (checkJobId !== undefined) {
-          checkJobId.infowindow.open({
-            anchor: checkJobId.marker,
+        // const checkJobId = jobMarkerList.find(j => j.id === job.id);
+          // console.log(checkJobId);
+        if (findJobId !== undefined) {
+          findJobId.infowindow.open({
+            anchor: findJobId.marker,
             map,
           })
         }
       }
       }>
-        {/* <Link to={`/job/${job.id}`} state={{ data: { job } }}> */}
         <JobCard job={job} />
-        {/* </Link> */}
       </li>
     )
   });
