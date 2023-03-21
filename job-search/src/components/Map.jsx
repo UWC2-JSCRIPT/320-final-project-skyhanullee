aaca4218a0865bcdbafd45a331f3467c1286649c
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-03-21 05:47:46 -0700

    cleaned up code

--- job-search/src/components/Map.jsx
+++ job-search/src/components/Map.jsx
@@ -1,9 +1,6 @@
-import { useRef, useEffect, useState, useContext } from "react";
-// import { renderToString } from 'react-dom/server';
+import { useRef, useEffect, useContext } from "react";
 import JobResultContext from '../context/JobResultContext';
 import MapContext from "../context/MapContext";
-// import Marker from "./Marker";
-// import MarkerCard from "./MarkerCard";
 
 function Map() {
   const ref = useRef();
@@ -19,63 +16,21 @@ function Map() {
     lng: jobResult.results[0]?.longitude || -95.7129
   }
 
-  // if(jobResult.results[0].latitude === undefined) {
-  //   center = {lat: 37.0902, lng: -95.7129};
-  //   console.log(`latitude is undefined`)
-  // }
-  // else {
-  //   center = {lat: jobResult.results[0].latitude, lng: jobResult.results[0].longitude};
-  // }
-
   useEffect(() => {
     // let map = new window.google.maps.Map(ref.current, {center, zoom});
     setMap(new window.google.maps.Map(ref.current, {center, zoom}));
-    console.log('marker useeffect called in Map.jsx');
+    // console.log('marker useeffect called in Map.jsx');
     jobResult.results.forEach((job)   => {
-    // // jobResult.results.map((job) => {
-    //   // console.log(job);
       if(job.latitude !== undefined && map !== undefined) {
-    //     addMarker(job, map, bounds);
         const jobLatLng = new window.google.maps.LatLng(job.latitude, job.longitude);
         bounds.extend(jobLatLng);
         map.fitBounds(bounds);
         setBounds(bounds);
-    //     // return (<Marker job={job} />);
       }
-    //   // return (console.log('job does not have latitude'));
     });
-    console.log('map called');
+    // console.log('map called');
 
   }, [jobResult]);
-
-  // useEffect(() => {
-  //   jobResult.results.forEach((job) => {
-  //     // console.log(job.latitude);
-  //     // if (job.latitude !== undefined && map !== undefined && bounds !== undefined) {
-  //     //   addMarker(job, map, bounds);
-  //     // }
-  //     if (job.latitude === undefined) {
-  //       console.log('job.latitude is undefined');
-  //       return
-  //     }
-  //     if (map === undefined) {
-  //       console.log('map is undefined');
-  //       return
-  //     }
-  //     if (bounds === undefined) {
-  //       console.log('bounds is undefined');
-  //       return
-  //     }
-  //     addMarker(job, map, bounds);
-  //   });
-  // },[jobResult.results, map])
-
-  // jobResult.results.forEach((job) => {
-  //   // console.log(job.latitude);
-  //   if(job.latitude !== undefined && map !== undefined) {
-  //     addMarker(job, map, setMap);
-  //   }
-  // });
 
   return (
     <div ref={ref} id="map" />
